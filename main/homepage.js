0b38ac199fbc240e18df7a9e7b03ea6eccf0abcf
chuy90 <jesus.almeida.@gmail.com>
2021-02-11 19:16:00 -0700

    cf-16-styling-bootstrap-cards

--- main/homepage.js
+++ main/homepage.js
@@ -3,7 +3,7 @@ function getAlbums() {
        .then(response => response.json())
        .then(data => {
               data.forEach(album => {
-                     $('.grid').append('<div class="col-12 col-md-4 block"> <div class="image-container"> <img src="./img/imgblog.png" alt="image 1"> <div class="text-block"> <p class="upper">'+ album.title +'</p> <p class="down">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa magnam modi </p> </div> </div> </div>');
+                     $('.grid').append('<div class=" col-12 col-md-4"> <div class="card text-center album-container"> <div class="card-body"> <h5 class="card-title">'+album.title+'</h5>  <a href="#" class="btn btn-primary">View this album</a> </div> </div> </div>');
               });
        })
 }
