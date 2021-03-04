ba7fdf9bbaf764411c3b6482679d7d42224e8b82
chuy90 <jesus.almeida.@gmail.com>
2021-03-04 16:52:43 -0700

    cf-12-sub-navigation

--- views/album/album.js
+++ views/album/album.js
@@ -7,9 +7,11 @@ function getPhotos(albumId) {
         } else {
             data.forEach(photo => {
                 $('.view-albums-container').append('<div class=" col-12"> <div class="card text-center album-container" "> <img class="card-img-top" src="'+photo.url+'" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">'+photo.title+'</h5> <a href="../../index.html" class="btn btn-primary">Go Back</a> </div> </div> </div>');
+            })
         }
     })
 }
+
 $(document).ready(function() {
     const parameters = new URLSearchParams(window.location.search)
     if (parameters.get('id')) {
