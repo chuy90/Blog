c0cb52f9e40a1ae2fa13c1e4cd6bbec04757cca7
chuy90 <jesus.almeida.@gmail.com>
2021-04-03 13:05:34 -0700

    cf-19-edit-delete-comments-textarea

--- views/post/post.js
+++ views/post/post.js
@@ -9,43 +9,47 @@ function getPost(id) {
 function getComments(id) {
     fetch('https://jsonplaceholder.typicode.com/comments?postId='+id)
     .then(response => response.json())
-    .then(commentData => { 
+    .then(commentData => {
             commentData.forEach(comment => {
-                $('.comments-container').append('<div class="col-12"> <div class="card text-center album-container"> <div class="card-body"> <h5 class="card-title">Name: '+
-                comment.name+
-                '</h5> <p> Id: '+
-                comment.id+
-                '</p> <p>E-mail: '+
-                comment.email+
-                '</p> <p>'+
-                comment.body+
-                `</p>
-                <div class="row"> 
-                <input type="text"  id="`+comment.id+`-editText" class="col-12 form-control edit-input"> </div> 
+                $('.comments-container').append(`<div id="comment-element-${comment.id}" class="col-12"> <div class="card text-center album-container"> <div class="card-body"> <h5 class="card-title">Name:
+                ${comment.name}
+                </h5> <p> Id:
+                ${comment.id}
+                </p> <p>E-mail:
+                ${comment.email}
+                </p> <p>
+                ${comment.body}
+                </p>
+                <div class="edit-form" id="edit-form-${comment.id}" style="display:none">
+                <div class="row d-flex" >
+                <textarea class="col-10" id="edit-text-${comment.id}" rows="3">
+                ${comment.body}</textarea>
+                <button class="enter-edit col-2" data-id="${comment.id}">Enter</button>
+                </div></div>
                 <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Edit </button> <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"  >
-                <a class="dropdown-item editTitle" onClick="editTitle(`+comment.id+`)">Title</a> 
-                <a class="dropdown-item editEmail" onClick="editEmail(`+comment.id+`)">Email</a> 
-                <a class="dropdown-item editComment" onClick="editComment(`+comment.id+`)">Comment</a> 
-                <button type="button" class="btn btn-danger delete-comment" onClick="deleteComment(`+comment.id+`)>Delete comment</button>
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Edit </button> 
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" >
+                <a href="#!" class="dropdown-item edit-comment" data-id="${comment.id}">Edit</a> 
+                <a href="#!" type="button" class="dropdown-item delete-comment" 
+                data-id="${comment.id}">Delete comment</a>
                 </div>  </div>  </div> </div>   </div> `);
             });
         })
 }
 
 
-$(document).on('click','.post-button',function() {
-  var title = document.getElementById('commentTitle').value;
-  var body = document.getElementById('commentBody').value;
-  var email = document.getElementById('commentEmail').value;
+$(document).on('click', '.post-button', function() {
+  var name = $("#add-name").val();
+  var email = $("#add-email").val();
+  var comment = $("#add-body").val();
 
   fetch('https://jsonplaceholder.typicode.com/comments', {
     method: 'POST',
     body: JSON.stringify({
       postId: 1,
-      title: title,
+      name: name,
       email: email,
-      body: body,
+      body: comment,
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
@@ -55,42 +59,18 @@ $(document).on('click','.post-button',function() {
     .then((json) => console.log(json));  
 });
 
-function editTitle(id) {
-  var textToEdit = document.getElementById(""+id+"-editText").value;
+$(document).on('click', '.edit-comment', function() {
+  const txtId = $(this).attr('data-id');
+  $(`#edit-form-${txtId}`).css("display","block");
+
+});
+
+$(document).on('click', '.enter-edit', function() {
+
+  const txtId = $(this).attr('data-id');
+  var textToEdit = $(`#edit-text-${txtId}`).val();
   
-  fetch('https://jsonplaceholder.typicode.com/comments/'+id, {
-    method: 'PATCH',
-    body: JSON.stringify({
-      title: textToEdit,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
-
-function editEmail(id) {
-  var textToEdit = document.getElementById(""+id+"-editText").value;
-
-  fetch('https://jsonplaceholder.typicode.com/comments/'+id, {
-    method: 'PATCH',
-    body: JSON.stringify({
-      email: textToEdit,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
-
-function editComment(id) {
-  var textToEdit = document.getElementById(""+id+"-editText").value;
-  
-  fetch('https://jsonplaceholder.typicode.com/comments/'+id, {
+  fetch('https://jsonplaceholder.typicode.com/comments/'+txtId, {
     method: 'PATCH',
     body: JSON.stringify({
       body: textToEdit,
@@ -101,16 +81,19 @@ function editComment(id) {
   })
     .then((response) => response.json())
     .then((json) => console.log(json));
-}
+});
 
-function deleteComment(id) {
-  fetch('https://jsonplaceholder.typicode.com/comments/'+id, {
+
+$(document).on('click', '.delete-comment', function() {
+  const commentId = $(this).attr('data-id');
+  fetch('https://jsonplaceholder.typicode.com/comments/'+commentId, {
     method: 'DELETE',
-  });
-}
+  })
+  .then((response) => response.json())
+  .then(() => $(`#comment-element-${commentId}`).remove());
+});
 
 $(document).ready(function() {
-    $('.editForm').hide();
     const parameters = new URLSearchParams(window.location.search)
     if (parameters.get('id')) {
         const id = parameters.get('id');
